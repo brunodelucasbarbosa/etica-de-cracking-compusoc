ea40dfc7b4c19e968cd1c28691ff8610b4861155
Bruno <brunnobarbosas@gmail.com>
2021-11-09 22:36:42 -0300

    refatorando paginas para modulos

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import {bemVindo} from './bemVindo.js';
 const botaoEnviar = document.querySelector('.button-enviar');
 const containerDireita = document.querySelector('.container-interno-direita');
 const containerEsquerda = document.querySelector('.container-interno-esquerda');
@@ -20,14 +21,15 @@ botaoEnviar.addEventListener('click', () => {
   pessoa.senha = senha.value;
   pessoa.email = email.value;
   containerEsquerda.innerHTML = '';
-  containerDireita.innerHTML = `
+  containerDireita.innerHTML = bemVindo();
+  /*containerDireita.innerHTML = `
               <h2 class="title-interno-direita roboto">Bem-vindxs ao <span class="span-safespace">SAFESPACE</span></h2>        
               <span class="span-interno-direita-logado roboto">Prezamos por uma navegação segura e dados bem guardados!</span>
 
               <div class="botao-enviar">
                 <button class="button-enviar ir-para-perfil roboto white">IR PARA MEU PERFIL</button>
               </div>
-  `
+  `*/
   window.scrollTo(0, 0);
   botaoIrPerfil = document.querySelector('.button-enviar.ir-para-perfil');
 
@@ -86,14 +88,70 @@ botaoEnviar.addEventListener('click', () => {
               Anna Friedericka Schwarzelmüller & Débora Abdalla
             </span>
           </div>`;
-        containerDireita.innerHTML = ` <h2 class="title-interno-direita roboto">Estudantes</h2>
-          <div class="container-info-cuidados creditos">
-            <span class="nomes-alunos roboto">Bruno de Lucas Barbosa</span>
-            <span class="nomes-alunos roboto">Elis Marcela</span>
-            <span class="nomes-alunos roboto">Jamilson Júnior</span>
-            <span class="nomes-alunos roboto">Larissa Maiara</span>
-            <span class="nomes-alunos roboto">Lorena Roberta</span>
-          </div>`;
+        containerDireita.innerHTML = `<h2 class="title-interno-direita roboto">Estudantes</h2>
+        <div class="icons-creditos">
+        <span class="nomes-alunos roboto">Bruno de Lucas Barbosa</span>
+        <a href="https://github.com/brunobarbosa17">
+          <img src="https://img.icons8.com/color/48/000000/github--v3.png" />
+        </a>
+
+        <a href="https://www.linkedin.com/in/brunodelucasbarbosa/">
+          <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
+        </a>
+        <a href="https://www.instagram.com/o_brunobarbosa">
+          <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
+        </a>
+        <a href="https://www.youtube.com/channel/UC7MDYFpb2qb7gjg9oOpbJFA">
+          <img src="https://img.icons8.com/color/50/000000/youtube--v3.png" />
+        </a>
+      </div>
+      <div class="icons-creditos">
+        <span class="nomes-alunos roboto">Elis Marcela</span>
+        <a href="https://github.com/develis">
+          <img src="https://img.icons8.com/color/48/000000/github--v3.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/elismrclsa/">
+          <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
+        </a>
+        <a href="https://www.instagram.com/elismrclsa/">
+          <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
+        </a>
+      </div>
+      <div class="icons-creditos">
+        <span class="nomes-alunos roboto">Jamilson Júnior</span>
+        <a href="https://github.com/devruso">
+          <img src="https://img.icons8.com/color/48/000000/github--v3.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/jamilson-j%C3%BAnior-34b5341b2/">
+          <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
+        </a>
+        <a href="https://www.instagram.com/jamiruso/">
+          <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
+        </a>
+      </div>
+
+      <div class="icons-creditos">
+        <span class="nomes-alunos roboto">Larissa Maiara</span>
+        <a href="https://www.linkedin.com/in/larissa-maiara-v-111a13b3">
+          <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
+        </a>
+        <a href="https://instagram.com/canto_da_lary?utm_medium=copy_link">
+          <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
+        </a>
+      </div>
+      <div class="icons-creditos">
+        <span class="nomes-alunos roboto">Lorena Roberta</span>
+        <a href="https://github.com/lorenarbt">
+          <img src="https://img.icons8.com/color/48/000000/github--v3.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/lorena-roberta-4416ba1ba/">
+          <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
+        </a>
+        <a href="https://www.instagram.com/lorenarbt/">
+          <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
+        </a>
+      </div>
+    </div>`;
       })
     })
 
