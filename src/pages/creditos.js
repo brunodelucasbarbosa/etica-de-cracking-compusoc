f41111795f10755ccd07a8c094e31a04a2018012
Bruno <brunnobarbosas@gmail.com>
2021-11-10 11:50:32 -0300

    adicionando target blank aos creditos

--- src/pages/creditos.js
+++ src/pages/creditos.js
@@ -14,63 +14,63 @@ export function creditosDireita() {
   const direita = `<h2 class="title-interno-direita roboto">Estudantes</h2>
   <div class="icons-creditos">
   <span class="nomes-alunos roboto">Bruno de Lucas Barbosa</span>
-  <a href="https://github.com/brunobarbosa17">
+  <a href="https://github.com/brunobarbosa17" target="_blank">
     <img src="https://img.icons8.com/color/48/000000/github--v3.png" />
   </a>
 
-  <a href="https://www.linkedin.com/in/brunodelucasbarbosa/">
+  <a href="https://www.linkedin.com/in/brunodelucasbarbosa/" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
   </a>
-  <a href="https://www.instagram.com/o_brunobarbosa">
+  <a href="https://www.instagram.com/o_brunobarbosa" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
   </a>
-  <a href="https://www.youtube.com/channel/UC7MDYFpb2qb7gjg9oOpbJFA">
+  <a href="https://www.youtube.com/channel/UC7MDYFpb2qb7gjg9oOpbJFA" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/youtube--v3.png" />
   </a>
 </div>
 <div class="icons-creditos">
   <span class="nomes-alunos roboto">Elis Marcela</span>
-  <a href="https://github.com/develis">
+  <a href="https://github.com/develis" target="_blank">
     <img src="https://img.icons8.com/color/48/000000/github--v3.png" />
   </a>
-  <a href="https://www.linkedin.com/in/elismrclsa/">
+  <a href="https://www.linkedin.com/in/elismrclsa/" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
   </a>
-  <a href="https://www.instagram.com/elismrclsa/">
+  <a href="https://www.instagram.com/elismrclsa/" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
   </a>
 </div>
 <div class="icons-creditos">
   <span class="nomes-alunos roboto">Jamilson Júnior</span>
-  <a href="https://github.com/devruso">
+  <a href="https://github.com/devruso" target="_blank">
     <img src="https://img.icons8.com/color/48/000000/github--v3.png" />
   </a>
-  <a href="https://www.linkedin.com/in/jamilson-j%C3%BAnior-34b5341b2/">
+  <a href="https://www.linkedin.com/in/jamilson-j%C3%BAnior-34b5341b2/" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
   </a>
-  <a href="https://www.instagram.com/jamiruso/">
+  <a href="https://www.instagram.com/jamiruso/" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
   </a>
 </div>
 
 <div class="icons-creditos">
   <span class="nomes-alunos roboto">Larissa Maiara</span>
-  <a href="https://www.linkedin.com/in/larissa-maiara-v-111a13b3">
+  <a href="https://www.linkedin.com/in/larissa-maiara-v-111a13b3" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
   </a>
-  <a href="https://instagram.com/canto_da_lary?utm_medium=copy_link">
+  <a href="https://instagram.com/canto_da_lary?utm_medium=copy_link" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
   </a>
 </div>
 <div class="icons-creditos">
   <span class="nomes-alunos roboto">Lorena Roberta</span>
-  <a href="https://github.com/lorenarbt">
+  <a href="https://github.com/lorenarbt" target="_blank">
     <img src="https://img.icons8.com/color/48/000000/github--v3.png" />
   </a>
-  <a href="https://www.linkedin.com/in/lorena-roberta-4416ba1ba/">
+  <a href="https://www.linkedin.com/in/lorena-roberta-4416ba1ba/" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/linkedin.png" />
   </a>
-  <a href="https://www.instagram.com/lorenarbt/">
+  <a href="https://www.instagram.com/lorenarbt/" target="_blank">
     <img src="https://img.icons8.com/color/50/000000/instagram-new--v2.png" />
   </a>
 </div>
